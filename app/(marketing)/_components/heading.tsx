27d2dcb027d7ba98a4d95e19cc74e7f7b7c4a1b3
SiangYin <siangyin916@gmail.com>
2025-07-02 01:24:44 +0800

    wip landing page

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+
+export const Heading = () => {
+  return (
+    <div className='max-w-3xl space-y-4'>
+      <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
+        Capture Ideas & Clear Your Mind.
+      </h1>
+      <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
+        <span className='underline'>Next Note</span> offers a minimalist,
+        distraction-free environment to help you stay focused and organized —
+        without clutter or complexity distractions. <br />
+        Just you and your notes.
+      </h3>
+
+      <Button>
+        Start writing
+        <ArrowRight className='h-4 w-4 ml-2' />{' '}
+      </Button>
+    </div>
+  )
+}
