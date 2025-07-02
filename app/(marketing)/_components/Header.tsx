3fc104b1ff1e3e8d6d4587611c6dcc19b86d505d
SiangYin <siangyin916@gmail.com>
2025-07-02 23:33:56 +0800

    add Spinner, user auth

--- app/(marketing)/_components/Header.tsx
+++ app/(marketing)/_components/Header.tsx
@@ -1,25 +1,46 @@
 'use client'
+import { useConvexAuth } from 'convex/react'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
+import { SignInButton } from '@clerk/clerk-react'
 
 import { Button } from '@/components/ui/button'
-import { ArrowRight } from 'lucide-react'
+import Spinner from '@/components/common/Spinner'
 
 export const Header = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
         Capture Ideas & Clear Your Mind.
       </h1>
       <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
-        <span className='underline'>Next note</span> offers a minimalist,
-        distraction-free environment to help you stay focused and organized —
-        without clutter or complexity distractions. <br />
+        <strong>Next note</strong> offers a minimalist, distraction-free
+        environment to help you stay focused and organized — without clutter or
+        complexity distractions. <br />
         Just you and your notes.
       </h3>
 
-      <Button>
-        Start noting
-        <ArrowRight className='h-4 w-4 ml-2' />{' '}
-      </Button>
+      {isLoading ? (
+        <div className='w-full flex justify-center items-center'>
+          <Spinner size='lg' />
+        </div>
+      ) : isAuthenticated ? (
+        <Button asChild>
+          <Link href='/documents'>
+            Enter Next note
+            <ArrowRight className='w-4 h-4 ml-2' />
+          </Link>
+        </Button>
+      ) : (
+        <SignInButton mode='modal'>
+          <Button>
+            Start noting
+            <ArrowRight className='w-4 h-4 ml-2' />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,12 +1,20 @@
 'use client'
 
-import { useScrollTop } from '@/hooks/use-scroll-top'
+import { useConvexAuth } from 'convex/react'
 import { cn } from '@/lib/utils'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import Link from 'next/link'
+
+import { useScrollTop } from '@/hooks/use-scroll-top'
+
+import { Button } from '@/components/ui/button'
 import { Logo } from './Logo'
 import ModeToggle from '@/components/common/ModeToggle'
+import Spinner from '@/components/common/Spinner'
 
 const Navbar = () => {
   const scrolled = useScrollTop()
+  const { isAuthenticated, isLoading } = useConvexAuth()
 
   return (
     <div
@@ -17,7 +25,29 @@ const Navbar = () => {
     >
       <Logo />
       <div className='md:ml-auto md:justify-end flex gap-x-2 justify-between items-center w-full'>
-        login
+        {isLoading && <Spinner />}
+
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode='modal'>
+              <Button variant='ghost' size='sm'>
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode='modal'>
+              <Button size='sm'>Join Next note free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant='ghost' size='sm' asChild>
+              <Link href='/documents'>Enter Next note</Link>
+            </Button>
+            <UserButton />
+          </>
+        )}
+
         <ModeToggle />
       </div>
     </div>
