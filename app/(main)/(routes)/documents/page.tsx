5ee86e005a4e9de485c2e1a93cbdd81c38a9acd2
SiangYin <siangyin.work@gmail.com>
2025-07-15 20:19:58 +0800

    Update page.tsx

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,46 +1,49 @@
-'use client'
+"use client"
 
-import Image from 'next/image'
-import { useUser } from '@clerk/clerk-react'
-import { Button } from '@/components/ui/button'
-import { PlusCircle } from 'lucide-react'
+import Image from "next/image"
+import { useUser } from "@clerk/clerk-react"
+import { Button } from "@/components/ui/button"
+import { PlusCircle } from "lucide-react"
+import { useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { toast } from "sonner"
 
 const DocumentsPage = () => {
   const { user } = useUser()
-  // const create = useMutation(api.documents.create)
+  const create = useMutation(api.documents.create)
 
   const onCreate = () => {
-    // const promise = create({ title: 'Untitled' })
-    // toast.promise(promise, {
-    //   loading: 'Creating a new note...',
-    //   success: 'New note created',
-    //   error: 'Failed to create a new note',
-    // })
+    const promise = create({ title: "Untitled" })
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Failed to create a new note",
+    })
   }
 
   return (
-    <div className='h-full flex flex-col items-center justify-center space-y-3'>
+    <div className="h-full flex flex-col items-center justify-center space-y-3">
       <Image
-        className='dark:hidden'
-        src='/empty.png'
-        alt='Empty'
-        width='300'
-        height='300'
+        className="dark:hidden"
+        src="/empty.png"
+        alt="Empty"
+        width="300"
+        height="300"
       />
       <Image
-        className='hidden dark:block'
-        src='/empty-dark.png'
-        alt='Empty'
-        width='300'
-        height='300'
+        className="hidden dark:block"
+        src="/empty-dark.png"
+        alt="Empty"
+        width="300"
+        height="300"
       />
 
-      <h2 className='text-lg font-medium'>
+      <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Next note
       </h2>
 
       <Button onClick={onCreate}>
-        <PlusCircle className='w-4 h-4 mr-2' />
+        <PlusCircle className="w-4 h-4 mr-2" />
         Create note
       </Button>
     </div>
