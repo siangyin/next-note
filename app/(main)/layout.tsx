c92533a1b61c6823c641d5559e07824026bb6b6f
SiangYin <siangyin.work@gmail.com>
2025-07-19 21:23:16 +0800

    install zustand, update dialog z index, and command hook and component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,30 +1,30 @@
-'use client'
+"use client"
 
-import { useConvexAuth } from 'convex/react'
-import { redirect } from 'next/navigation'
-import Spinner from '@/components/common/Spinner'
-import Navigation from '@/components/main/Navigation'
-import SearchCommand from '@/components/main/SearchCommand'
+import { useConvexAuth } from "convex/react"
+import { redirect } from "next/navigation"
+import Spinner from "@/components/common/Spinner"
+import Navigation from "@/components/main/Navigation"
+import SearchCommand from "@/components/main/SearchCommand"
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth()
 
   if (isLoading) {
     return (
-      <div className='h-full flex justify-center items-center'>
-        <Spinner size='lg' />
+      <div className="h-full flex justify-center items-center">
+        <Spinner size="lg" />
       </div>
     )
   }
 
   if (!isAuthenticated) {
-    return redirect('/')
+    return redirect("/")
   }
 
   return (
-    <div className='h-full flex dark:bg-[#1F1F1F]'>
+    <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
-      <main className='flex-1 h-full overflow-y-auto'>
+      <main className="flex-1 h-full overflow-y-auto">
         <SearchCommand />
         {children}
       </main>
